622d6ebdf8c04106e0a16baf0d8b4f36341e2a47
sarah castorena <s.castorena@outlook.com>
2022-10-04 00:55:13 -0700

    New commit

--- q2.py
+++ q2.py
@@ -8,3 +8,15 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+userString = None
+previous = None
+larger = None
+Smaller = None
+while (userString != "stop") and (userString != "STOP"):
+     userString = input('Enter string, enter stop when finished: ')
+     if (previous is not None) and (len(userString < previous) and len(userString) < smaller):
+        smaller = userString
+     if (previous is not None) and (len(userString) > previous) and (len(userString) > larger):
+        larger = userString 
+previous = len(userString)         
+print(larger, smaller)
